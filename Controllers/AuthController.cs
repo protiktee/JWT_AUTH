25bf8f7244537aee26a092c2e9574011c56e81e7
Md. Nafiur Rahman Protik <protiktee@users.noreply.github.com>
2024-06-07 22:28:24 +0600

    jwt

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using JWT_AUTH_OST_4.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_AUTH_OST_4.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetServiceToken()
+        {
+            return Ok(ServiceToken.GenerateServiceToken());
+        }
+    }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_AUTH_OST_4.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet(Name = "Index"), Authorize]
+        public ActionResult Index()
+        { 
+            return Ok(new { ff = "this is private access" });
+        }
+        [HttpGet("QryFromUnauthArea")]
+        public ActionResult QryFromUnauthArea()
+        {
+
+            return Ok(new { token = "this is public access" });
+        }
+
+    }
+}
